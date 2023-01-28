1b8445e98f8406501c5d70da893351ba07600118
KarimMaged <karim.maged2020@yahoo.com>
2023-01-28 17:45:41 +0200

    Add comments to the index.js file

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import Project from './project';
 import Todo from './Todo';
 import { replaceAddProj } from './sideBarUI';
 import projectIcon from './imgs/newProjIcon.svg';
+
 // the array that will store all projects
 const projectsLibrary = [];
 
 let projects = document.querySelector('#projects');
 
+/*  the sideUI Module is responsible for the input that will appear on the 
+sideBar, it will create a new project and append it to the projects div
+ */
 const sideUI = (function () {
+  //event listener for the add new project button
   document.querySelector('#newProj').addEventListener('click', () => {
     let newForm = replaceAddProj();
     let inputDiv = newForm[1].children;
 
+    /* event listener for the add button 
+       it will create a new div for the project that has an icon
+       and the project name. it will create a new Project and push it 
+       to the projectsLibrary array. it also checks the project name is
+       not empty
+    */
     inputDiv[1].addEventListener('click', () => {
       if (inputDiv[0].value !== '') {
         let newProj = document.createElement('div');
@@ -29,10 +40,19 @@ const sideUI = (function () {
         inputDiv[0].setAttribute('placeholder', 'project must have a name');
       }
     });
-
+    /* event listener for the cancel button that will remove the input div and 
+       recreate the add new project button
+    */
     inputDiv[2].addEventListener('click', () => {
       projects.append(newForm[0]);
       newForm[1].remove();
     });
   });
 })();
+
+//event listener for dynamically added projects
+document.addEventListener('click', (e) => {
+  if (e.target.getAttribute('class') === 'new-project') {
+    console.log('it worked');
+  }
+});
