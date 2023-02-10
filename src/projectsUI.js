522443eb5d73e50dce09c4f497a7adfb56c7299e
KarimMaged <karim.maged2020@yahoo.com>
2023-02-10 17:09:11 +0200

    Add a function that will check if a project has todos and will add the todos to the UI

--- src/projectsUI.js
+++ src/projectsUI.js
@@ -5,7 +5,31 @@ export default function createProjectUI(project) {
   let header = document.createElement('h1');
   header.textContent = project.name;
 
-  //will need more functionality to add todos here.....
+  //this function will create the contents of a todo if the project has todos
+  function todosUI() {
+    let todos = [];
+    if (project.todo.length > 0) {
+      project.todo.forEach((elem) => {
+        let div = document.createElement('div');
+        div.classList.add('single-todo');
+        let title = document.createElement('div');
+        title.textContent = elem.title;
+        let date = document.createElement('div');
+        date.textContent = elem.dueDate;
+        let priority = document.createElement('div');
+        priority.textContent = elem.priority;
+        if (elem.discription !== '') {
+          let discription = document.createElement('div');
+          discription.textContent = elem.discription;
+          div.append(title, date, priority, discription);
+        } else {
+          div.append(title, date, priority);
+        }
+        todos.push(div);
+      });
+    }
+    return todos;
+  }
 
   let newTodo = document.createElement('div');
   newTodo.classList.add('addNewTodo');
@@ -15,5 +39,5 @@ export default function createProjectUI(project) {
   divText.textContent = 'Add a todo';
   newTodo.append(icon, divText);
 
-  return [header, newTodo];
+  return [header, ...todosUI(), newTodo];
 }
